e9e852ef4d08824ea8799cd9b4a69c6af07966c6
andre <andrepedro2004@hotmail.com>
2025-07-29 00:49:18 +0100

    ready for the first build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,25 @@
+import java.net.URI
+
+fun loadEnvFile(path: String = ".env") {
+    File(path).forEachLine {
+        val (key, value) = it.split("=", limit = 2)
+        System.setProperty(key.trim(), value.trim())
+    }
+}
+
+// Carrega variáveis do arquivo .env
+loadEnvFile()
+
+fun getEnvOrProperty(name: String): String =
+    System.getenv(name) ?: System.getProperty(name) ?: error("Variable '$name' is missing")
+
+
 plugins {
     kotlin("jvm") version "2.0.21"
     `java-gradle-plugin`
     `kotlin-dsl`
     id("com.gradle.plugin-publish") version "1.3.1"
+    id("org.sonarqube") version "6.2.0.5505"
 }
 
 group = "pt.grupovissoma"
@@ -10,8 +27,21 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
-}
+    mavenLocal {
+        content {
+            includeGroup("pt.grupovissoma")
+        }
 
+    }
+}
+sonar {
+    properties {
+        property("sonar.host.url", getEnvOrProperty("SONAR_HOST_URL"))
+        property("sonar.projectKey", getEnvOrProperty("SONAR_PROJECT_KEY"))
+        property("sonar.projectName", "graphql-types-generator")
+        property("sonar.token", getEnvOrProperty("SONAR_TOKEN"))
+    }
+}
 dependencies {
     implementation("io.github.classgraph:classgraph:4.8.181")
     implementation("com.squareup:kotlinpoet:2.2.0")
@@ -20,7 +50,7 @@ dependencies {
     implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
     testImplementation(platform("org.junit:junit-bom:5.13.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.gradle:test-kit")
+//    testImplementation("org.gradle:test-kit")
     testImplementation(kotlin("test"))
 }
 
@@ -41,6 +71,24 @@ gradlePlugin {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.publish {
+    apply(plugin = "maven-publish")
+
+    publishing {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = URI.create("https://maven.pkg.github.com/grupo-vissoma/graphql-kotlin")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
+}
+
 kotlin {
     jvmToolchain(21)
 }
